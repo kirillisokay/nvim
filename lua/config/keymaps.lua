d77176a157010fe6fed08e0ce4fe36918b247634
K.Y. <kirillisokay@icloud.com>
2025-03-11 18:06:10 +0200

    Update some things

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -64,9 +64,9 @@ keymap.set("n", "<C-j>", function()
 end, opts)
 
 keymap.set("n", "<leader>r", function()
-  require("craftzdog.hsl").replaceHexWithHSL()
+  require("kirillisokay.hsl").replaceHexWithHSL()
 end)
 
 keymap.set("n", "<leader>i", function()
-  require("craftzdog.lsp").toggleInlayHints()
+  require("kirillisokay.lsp").toggleInlayHints()
 end)
